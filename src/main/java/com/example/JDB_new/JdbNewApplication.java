781d399ef5c27b4c306dfc385c276e67806d6e5f
Ali Srour <alisrourm13@gmail.com>
2024-10-04 22:50:35 +0300

    Adding initial files

---
+++ src/main/java/com/example/JDB_new/JdbNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.JDB_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JDB_new/JdbNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JDB_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
